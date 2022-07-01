a1404540ee25faddd2b0922c6192359ac3a2e45b
sebazzz6 <sebastian.zuniga.lob@gmail.com>
2022-07-01 16:54:35 -0400

    INFO: set format money in total price

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ formulario.addEventListener("submit", (e) =>{
     let color = document.querySelector("#color").value;
     let valor = document.querySelector("#precio").value;
     let cantidad = document.querySelector("#cant").value;
-    document.querySelector("#total").innerHTML = '$' + (cantidad * valor) + ' => Precio Ingresado: $' + valor;
+    document.querySelector("#total").innerHTML = '$' + (cantidad * valor).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + ' => Precio Ingresado: $' + valor.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
     document.querySelector("#cantidadSel").innerHTML = cantidad;
     document.querySelector("#circulo").style.backgroundColor = color;
     document.querySelector("#imagenNotebook").src = 'assets/img/note'+color+'.png';
