1d36fc9d746698d2cce8d89012642ba9f26b5823
sebazzz6 <sebastian.zuniga.lob@gmail.com>
2022-07-01 17:25:48 -0400

    INFO: set format money in total price with toLocaleString()

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ formulario.addEventListener("submit", (e) =>{
     let color = document.querySelector("#color").value;
     let valor = document.querySelector("#precio").value;
     let cantidad = document.querySelector("#cant").value;
-    document.querySelector("#total").innerHTML = '$' + (cantidad * valor).toLocaleString() + ' => Precio Ingresado: $' + Number(valor).toLocaleString();
+    document.querySelector("#total").innerHTML = '$' + Number(cantidad * valor).toLocaleString() + ' => Precio Ingresado: $' + Number(valor).toLocaleString();
     document.querySelector("#cantidadSel").innerHTML = cantidad;
     document.querySelector("#circulo").style.backgroundColor = color;
     document.querySelector("#imagenNotebook").src = 'assets/img/note'+color+'.png';
